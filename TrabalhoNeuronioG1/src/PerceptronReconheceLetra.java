d7e438c75464331978e3ddb5216aeb4c73611fa2
Diego <diegofeijo.silva@gmail.com>
2015-03-17 21:32:57 -0300

    a

--- TrabalhoNeuronioG1/src/PerceptronReconheceLetra.java
+++ TrabalhoNeuronioG1/src/PerceptronReconheceLetra.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class PerceptronReconheceLetra {
 	public static void main(String args[]) {
-		NeuronioLetra neuronio = new NeuronioLetra();
 
-		int x1[] = new int[4]; // 1a entrada
-		int x2[] = new int[4]; // 2a entrada
-		// int d[] = new int[4]; // saida desejada
+		NeuronioLetra neuronio = new NeuronioLetra();
 
 		int X1[][] = new int[4][3]; // ENTRADA
 		int D[][] = new int[4][3]; // SAÍDA
@@ -53,7 +50,7 @@ public class PerceptronReconheceLetra {
 		double eta = 1d;
 		// eta é a constante (taxa) de aprendizagem
 
-		System.out.println("--- TREINAMENTO");
+		System.out.println("--- TREINAMENTO LETRA L");
 		while (true) {
 			epocas++;
 			erroGeral = 0;
@@ -74,7 +71,76 @@ public class PerceptronReconheceLetra {
 					// ajuste dos pesos
 					if (erro != 0) {
 						neuronio.setW0(neuronio.getW0() + eta * erro);
-						neuronio.setW1(neuronio.getW1() + eta * erro * x1[i]);
+						neuronio.setW1(neuronio.getW1() + eta * erro * X1[i][j]);
+					}
+					System.out.println("Neuronio - pesos: " + neuronio);
+					erroGeral = erroGeral + abs(erro);
+
+				}
+
+			}
+
+			// pára quando para todas as entradas o erro for zero
+			if (erroGeral == 0)
+				break;
+		}
+
+		// Conjunto de Treino : LETRA T
+		X1[0][0] = 1;
+		X1[0][1] = 1;
+		X1[0][2] = 1;
+
+		X1[1][0] = 0;
+		X1[1][1] = 1;
+		X1[1][2] = 0;
+
+		X1[2][0] = 0;
+		X1[2][1] = 1;
+		X1[2][2] = 0;
+
+		X1[3][0] = 0;
+		X1[3][1] = 1;
+		X1[3][2] = 0;
+
+		// SAÍDA
+		D[0][0] = 1;
+		D[0][1] = 1;
+		D[0][2] = 1;
+
+		D[1][0] = 0;
+		D[1][1] = 1;
+		D[1][2] = 0;
+
+		D[2][0] = 0;
+		D[2][1] = 1;
+		D[2][2] = 0;
+
+		D[3][0] = 0;
+		D[3][1] = 1;
+		D[3][2] = 0;
+
+		System.out.println("--- TREINAMENTO LETRA T");
+		while (true) {
+			epocas++;
+			erroGeral = 0;
+
+			System.out.println("Epoca: " + epocas);
+
+			// PERCORRE AS LINHAS
+			for (i = 0; i < 4; i++) {
+
+				for (j = 0; j < 3; j++) {
+
+					// propagação
+					y = neuronio.calculaY(X1[i][j]);
+
+					// calcula do erro
+					erro = D[i][j] - y;
+
+					// ajuste dos pesos
+					if (erro != 0) {
+						neuronio.setW0(neuronio.getW0() + eta * erro);
+						neuronio.setW1(neuronio.getW1() + eta * erro * X1[i][j]);
 					}
 					System.out.println("Neuronio - pesos: " + neuronio);
 					erroGeral = erroGeral + abs(erro);
@@ -93,21 +159,31 @@ public class PerceptronReconheceLetra {
 		Scanner dados = new Scanner(System.in);
 		System.out.println("\n--- GENERALIZACAO");
 
-		while (true) {
-			// digita novas entradas
-			System.out.println("Digite -100 para encerrar");
-			System.out.print("Digite a entrada (x1): ");
-			entrada1 = dados.nextInt();
-			if (entrada1 == -100)
-				break;
+		// TESTANDO A REDE
+		X1[0][0] = 1;
+		X1[0][1] = 0;
+		X1[0][2] = 0;
 
-			System.out.print("Digite a entrada (x2): ");
-			entrada2 = dados.nextInt();
-			if (entrada2 == -100)
-				break;
+		X1[1][0] = 1;
+		X1[1][1] = 0;
+		X1[1][2] = 0;
 
-			// propagação
-			//System.out.println("Saida Gerada pela rede: " + neuronio.calculaY(entrada1, entrada2));
+		X1[2][0] = 1;
+		X1[2][1] = 0;
+		X1[2][2] = 0;
+
+		X1[3][0] = 0;
+		X1[3][1] = 0;
+		X1[3][2] = 0;
+
+		// PERCORRE AS LINHAS
+		for (i = 0; i < 4; i++) {
+
+			for (j = 0; j < 3; j++) {
+				// propagação
+				System.out.println("Saida POS: "+ i + "-"+ j + " = " + neuronio.calculaY(X1[i][j]));
+			}
 		}
+
 	}
 }
